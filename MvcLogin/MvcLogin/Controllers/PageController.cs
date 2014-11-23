fc8a2d2d95014b60f9c9171c01f43cc1688ed160
Deepan <deepan.darshan@gmail.com>
2014-11-23 17:23:56 +0530

    commit1

---
+++ MvcLogin/MvcLogin/Controllers/PageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLogin.Controllers
+{
+    public class PageController : Controller
+    {
+        //
+        // GET: /Page/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AfterLogin()
+        {
+            if (Session["LogedUsername"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login","Login");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Login");
+        }
+
+    }
+}
